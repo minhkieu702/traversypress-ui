8d43dd001ba16d2a62dc0f36e065b044b7621fa8
minhkieu702 <115796407+minhkieu702@users.noreply.github.com>
2024-10-30 16:14:48 +0700

    update

--- app/(main)/fish/edit/[id]/page.tsx
+++ app/(main)/fish/edit/[id]/page.tsx
@@ -17,8 +17,6 @@ import { Input } from "@/components/ui/input";
 import BackButton from "@/components/BackButton";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
-import { BreedType } from "@/types/ResponseModel/BreedType";
-import { ProductType } from "@/types/ResponseModel/ProductType";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -26,6 +24,8 @@ import { useFieldArray, useForm } from "react-hook-form";
 import { z } from "zod";
 import { Textarea } from "@/components/ui/textarea";
 import { AxiosError, AxiosResponse } from "axios";
+import { BreedType } from "@/types/ResponseModel/BreedType";
+import { ProductType } from "@/types/ResponseModel/ProductType";
 import { AwardType } from "@/types/ResponseModel/AwardType";
 
 const formSchema = z.object({
